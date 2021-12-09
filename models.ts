d0edc5d1dd4b12aed4752cf72ac9991d71871eaa
Miha Žnidar <mayki.znidar@gmail.com>
2021-12-09 13:04:43 +0100

    Added api calling and interfaces from swagger.

--- models.ts
+++ models.ts
@@ -1,4 +1,3 @@
-
 export interface Party {
     categories: [];
     creationDate: Date;
@@ -6,7 +5,6 @@ export interface Party {
     id: string;
     info: Info;
 }
-
 export interface Info{
     name: String;
     description: String;
@@ -15,4 +13,42 @@ export interface Info{
     dateFrom: Date;
     dateTo: Date;
     budget: Number;
+}
+export interface GuestBody{
+    id: String;
+    guests: Guest[];
+}
+export interface GuestList{
+    guests: Guest[];
+}
+export interface Guest{
+    guestId: Number;
+    name: String;
+    surname: String;
+    email: String;
+    phone: String;
+    vegan: boolean;
+    vegetarian: boolean;
+    nonDrinker: boolean;
+    paid:boolean;
+    host:boolean;
+}
+export interface CategoryBody{
+    id: String;
+    categories: Category[];
+}
+export interface Category{
+    categoryId: Number;
+    name: String;
+    description: String;
+    budgetPercentage: Number;
+    proposedTotal: Number;
+    items: Item[];
+}
+export interface Item{
+    itemId: Number;
+    name: String;
+    description: String;
+    price: Number;
+    quantity: Number;
 }
