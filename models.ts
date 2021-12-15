f2c46bb650c604e46634e3da1f30cdfa56c834ca
DESKTOP-UJGUQ3V\Masina <mayki.znidar@gmail.com>
2021-12-15 16:10:16 +0100

    Fixed rendering before calculation

--- models.ts
+++ models.ts
@@ -52,3 +52,8 @@ export interface Item{
     price: Number;
     quantity: Number;
 }
+
+export interface CategoryBudget{
+    name:String;
+    sum:Number;
+}
--- screens/calculateScreen.tsx
+++ screens/calculateScreen.tsx
@@ -1,9 +1,9 @@
 import * as React from 'react';
 import { SafeAreaView,ScrollView,View,Text,StyleSheet,Image } from 'react-native';
-import { Col, Row, Grid } from "react-native-easy-grid";
+import { Col, Row } from "react-native-easy-grid";
 
 import {getPartyById} from '../services/ppRest';
-import {Party, Category, Item} from '../models';
+import {Party, Category, Item, CategoryBudget} from '../models';
 
 interface PartyProps{
   navigation: any;
@@ -15,36 +15,21 @@ export default function CalculateScreen(props: PartyProps) {
     
     const partyId = props.route.params.id;
     const [party, setParty] = React.useState<Party>()
-
-    const [location, setLocation] = React.useState(0)
-    const [food, setFood] = React.useState(0)
-    const [drinks, setDrinks] = React.useState(0)
-    const [people, setPeople] = React.useState(0)
-    const [total, setTotal] = React.useState(0)
     const [guests, setGuests] = React.useState(0)
+    const [categoryBudget, setCategoryBudget] = React.useState<CategoryBudget[]>([])
 
-    
-    let categoryBudget: Array<{name: any, sum: any}> = []
     let totalBudget = 0
     let perPerson = 0
     
     async function getPartyData() {
-        // TODO Check if party name not null and show error
-        const response = await getPartyById(partyId)
+
+        const response:Party = await getPartyById(partyId)
+
         if (response){
             setParty(response)
-            console.log(response)
-        }
-        // TODO Show error ...
-    }
+            setGuests(response.guests.length)
 
-
-    function calculate(){
-
-        const categories:Category[] | undefined = party?.categories
-
-        if(categories){
-
+            const categories:Category[] = response.categories
             categories.forEach((category:Category) => {
                 
                 var sum: number = 0;
@@ -56,11 +41,11 @@ export default function CalculateScreen(props: PartyProps) {
                 })
                 const name = category.name;
 
-                categoryBudget.push({name, sum})
+                setCategoryBudget([...categoryBudget,{name,sum}])
                 totalBudget += sum
             });
-        totalBudget += Number(party?.info.budget)
-        perPerson = party?.guests.length ? totalBudget/party.guests.length : 0
+            totalBudget += Number(party?.info.budget)
+            perPerson = guests ? totalBudget / guests : 0
         }
     }
 
@@ -68,68 +53,63 @@ export default function CalculateScreen(props: PartyProps) {
         getPartyData()
     }, []);
 
-    React.useEffect(() => {
-        calculate()
-    }, [party]);
+    return (
+        <SafeAreaView style={styles.page}>
+            <ScrollView style={styles.container}>
+                    <Text style={styles.text}>PLAN FOR:</Text>
+                    <Text style={styles.title}>{party?.info.name} party</Text>
+                    <View
+                        style={{
+                            borderBottomColor: 'white',
+                            borderBottomWidth: 1,
+                        }}
+                    />
+                    <Row style={styles.spacing}>
+                        <Col size={130}>
+                            <Text style={styles.nameText}>
+                                <Text style={styles.price}>{party?.info.budget}€</Text>
+                            </Text>
+                        </Col>
+                        <Col size={30} >
+                            <Image source={require('../assets/images/Pin.png')} style={{flex: 1, width: 100, resizeMode: 'contain'}}/>
+                        </Col>
+                    </Row>
+                    {categoryBudget.map((category,key)=>{
+                        return(
+                            <Row style={styles.spacing} key={key}>
+                                <Col size={130}>
+                                    <Text style={styles.nameText}>{category.name}
+                                        <Text style={styles.price}> {category.sum}€</Text>
+                                    </Text>
+                                </Col>
+                                <Col size={30} >
+                                    <Image source={require('../assets/images/Beer.png')} style={{flex: 1, width: 100, resizeMode: 'contain'}}/>
+                                </Col>
+                            </Row>
+                        )
+                    })}
+                    <Row style={styles.spacing}>
+                        <Col size={130}>
+                            <Text style={styles.nameText}>People invited 
+                                <Text style={styles.price}> {guests} </Text>
+                            </Text>
+                        </Col>
+                        <Col size={30} >
+                            <Image source={require('../assets/images/Person.png')} style={{flex: 1, width: 100, resizeMode: 'contain'}}/>
+                        </Col>
+                    </Row>
+            </ScrollView>
 
-  return (
-    <View style={styles.page}>
-        <View style={styles.container}>
-                <Text style={styles.text}>PLAN FOR:</Text>
-                <Text style={styles.title}>{party?.info.name} party</Text>
-                <View
-                    style={{
-                        borderBottomColor: 'white',
-                        borderBottomWidth: 1,
-                    }}
-                />
-                <Row style={styles.spacing}>
-                    <Col size={130}>
-                        <Text style={styles.nameText}>
-                            <Text style={styles.price}>{location}€</Text>
-                        </Text>
-                    </Col>
-                    <Col size={30} >
-                        <Image source={require('../assets/images/Pin.png')} style={{flex: 1, width: 100, resizeMode: 'contain'}}/>
-                    </Col>
-                </Row>
-                {categoryBudget.map((category,key)=>{
-                    return(
-                        <Row style={styles.spacing}>
-                            <Col size={130}>
-                                <Text style={styles.nameText}>{category.name}
-                                    <Text style={styles.price}> {category.sum}€</Text>
-                                </Text>
-                            </Col>
-                            <Col size={30} >
-                                <Image source={require('../assets/images/Beer.png')} style={{flex: 1, width: 100, resizeMode: 'contain'}}/>
-                            </Col>
-                        </Row>
-                    )
-                })}
-
-                <Row style={styles.spacing}>
-                    <Col size={130}>
-                        <Text style={styles.nameText}>People invited 
-                            <Text style={styles.price}> {people} </Text>
-                        </Text>
-                    </Col>
-                    <Col size={30} >
-                        <Image source={require('../assets/images/Person.png')} style={{flex: 1, width: 100, resizeMode: 'contain'}}/>
-                    </Col>
-                </Row>
-        </View>
-
-        <View style={styles.totalContainer}>
-                <Text style={styles.totalTitle}>TOTAL</Text>
-                <Text style={styles.total}>TOTAL {total}€</Text>
-                <Text style={styles.total}>GUESTS {guests}</Text>
-                <Text style={styles.total}>PER PERSON {people / total}€</Text>
-        </View>
-        
-    </View>
-  );
-}
+            <View style={styles.totalContainer}>
+                    <Text style={styles.totalTitle}>TOTAL</Text>
+                    <Text style={styles.total}>TOTAL {totalBudget}€</Text>
+                    <Text style={styles.total}>GUESTS {guests}</Text>
+                    <Text style={styles.total}>PER PERSON {totalBudget / guests}€</Text>
+            </View>
+            
+        </SafeAreaView>
+    );
+    }
 
 const styles = StyleSheet.create({
     container:{
